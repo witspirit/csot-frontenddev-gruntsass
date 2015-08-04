363f02ee3477fd7960db71119cce99a2eb131230
oak3 <oak3@oak3s-iMac-4.local>
2015-08-04 14:59:47 +0200

    Initial commit, including the main result of the exercise

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    clean: {
+        dev : ['build','dist'],
+        full : ['build', 'dist', '.sass-cache', 'bower_components', 'node_modules']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    sass: {
+      dist : {
+        files : {
+          'build/styles/main.css' : 'app/styles/*.scss'
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/scripts/main.min.js' : 'app/scripts/*.js'
+        }
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'dist/styles/main.min.css' : 'build/styles/main.css'
+        }
+      }
+    },
+    copy: {
+      app: {
+        files: [
+          {
+            expand: true,
+            src: ['app/*.html'],
+            dest: 'dist/',
+            flatten: true
+          }
+        ]
+      },
+      deps: {
+        files: [
+          {
+            expand: true,
+            src: ['bower_components/**'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      js: {
+        files: ['app/scripts/*.js'],
+        tasks: ['jshint', 'uglify']
+      },
+      css: {
+        files: ['app/styles/*.scss'],
+        tasks: ['sass','cssmin']
+      },
+      html: {
+        files: ['app/*.html'],
+        tasks: ['copy:app']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('build', ['jshint', 'sass', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['clean:dev', 'build', 'watch']);
+};
---
+++ app/scripts/main.js
@@ -0,0 +1,1 @@
+console.log('Welcome to the modern front-end development workshop');
